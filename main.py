042cfc4e708abd83eaf207b6c51a9cf35ed38f31
chrisalbon <3179476+tradinscd@users.noreply.github.com>
2024-03-29 15:55:12 -0700

    added bootstrap template

--- main.py
+++ main.py
@@ -1,20 +1,16 @@
 from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
 
-# __name__ determines the root directory for the application
+
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
 
 
-# The association between a URL and the function that handles it is called
-# a route. The function index is called a view function.
 @app.route("/")
 def index():
-    @app.route("/")
-    def index():
-        return render_template("index.html")
-
-    return "<h1>Hello World!</h1>"
+    return render_template("index.html")
 
 
 @app.route("/user/<name>")
 def user(name):
-    return "<h1>Hello, {}!</h1>".format(name)
+    return render_template("user.html", name=name)
