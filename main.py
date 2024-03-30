e73b3b80766ac28a246319fb91e0ae005f8e91e1
chrisalbon <3179476+tradinscd@users.noreply.github.com>
2024-03-30 10:54:19 -0700

    added forms and flashing

--- main.py
+++ main.py
@@ -1,16 +1,30 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session, redirect, url_for, flash
 from flask_bootstrap import Bootstrap
+from dotenv import load_dotenv
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+import os
 
-
+load_dotenv()
 app = Flask(__name__)
-bootstrap = Bootstrap(app)
+app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
+dbootstrap = Bootstrap(app)
 
 
-@app.route("/")
+class NameForm(FlaskForm):
+    name = StringField("What is your name?", validators=[DataRequired()])
+    submit = SubmitField("Submit")
+
+
+@app.route("/", methods=["GET", "POST"])
 def index():
-    return render_template("index.html")
-
-
-@app.route("/user/<name>")
-def user(name):
-    return render_template("user.html", name=name)
+    form = NameForm()
+    if form.validate_on_submit():
+        # flashes if new name submitted does not match previous name
+        old_name = session.get("name")
+        if old_name is not None and old_name != form.name.data:
+            flash("Looks like you have changed your name!")
+        session["name"] = form.name.data
+        return redirect(url_for("index"))
+    return render_template("index.html", form=form, name=session.get("name"))
